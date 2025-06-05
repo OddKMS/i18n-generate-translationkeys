b8fe2c483b108222394484c1248ba0ece169ea6d
Odd Karsten Svartaas <oksvart@gmail.com>
2025-06-05 12:52:53 +0200

    Expand and improve getConfiguration
    Add config parameter filenameOverride so the user can control the name of the file and object within

--- src/helpers/configuration.ts
+++ src/helpers/configuration.ts
@@ -3,7 +3,15 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import fs from 'fs';
 
-function getConfiguration() {
+type configuration = {
+  i18nLocation: string;
+  translations: string;
+  output: string;
+  filenameOverride: string;
+  quiet: boolean;
+};
+
+function getConfiguration(): configuration {
   // CLI Arguments
   const cliArgs = yargs(hideBin(process.argv))
     .options({
@@ -19,7 +27,13 @@ function getConfiguration() {
       output: {
         alias: 'o',
         type: 'string',
-        description: 'Output location of the translationKeys file.',
+        description:
+          'Path to the output directory of the translationKeys.ts file.',
+      },
+      filenameOverride: {
+        alias: 'f',
+        type: 'string',
+        description: 'Overrides the file and object name of the output.',
       },
       quiet: {
         alias: 'q',
@@ -27,11 +41,20 @@ function getConfiguration() {
         description: 'Supress output from the script.',
       },
     })
-    .parse();
+    .parseSync();
 
   // From config file
-  const configFileName = '.tkrc.json';
-  const configFile = () => {
+  function getConfigFromFile():
+    | {
+        i18nLocation: string;
+        translationsLocation: string;
+        output: string;
+        filenameOverride: string;
+        quiet: boolean;
+      }
+    | undefined {
+    const configFileName = '.tkrc.json';
+
     if (fs.existsSync(configFileName)) {
       try {
         const configData = fs.readFileSync(configFileName, 'utf-8');
@@ -43,26 +66,54 @@ function getConfiguration() {
     } else {
       return undefined;
     }
-  };
+  }
+
+  const configFile = getConfigFromFile();
 
   // Default values
   const i18nLocationDefault = './src/i18n';
   const translationsLocationDefault = i18nLocationDefault + '/translations';
-  const outputLocationDefault = i18nLocationDefault + '/translationKeys.ts';
+  const filenameDefault = '/translationKeys.ts';
+  const outputLocationDefault = i18nLocationDefault;
   const quietDefault = false;
 
   // Parameter priority is as follows:
   // CLI Argument > Config File > Default Values
+  const i18nLocation =
+    cliArgs.i18nLocation ?? configFile?.i18nLocation ?? i18nLocationDefault;
+
+  const translations =
+    cliArgs.translations ??
+    configFile?.translationsLocation ??
+    translationsLocationDefault;
+
+  let output = cliArgs.output ?? configFile?.output ?? outputLocationDefault;
+
+  // If the output path is erroneously provided with a filename/ending
+  // we remove the filename and display an message informing the user
+  // of the correct way to change the filename
+  const outputHasFileEnding: RegExp = /^.*\.(.*)$/gm;
+  if (outputHasFileEnding.test(output)) {
+    console.warn('Filename detected in output parameter.');
+    console.warn(
+      'The supplied filename will be ignored and default filename will be used.'
+    );
+    console.warn(
+      'To output to a different file use the --filenameOverride parameter.'
+    );
+  }
+
+  const filenameOverride =
+    cliArgs.filenameOverride ?? configFile?.filenameOverride ?? filenameDefault;
+
+  const quiet = cliArgs.quiet ?? configFile?.quiet ?? quietDefault;
+
   return {
-    i18nLocation:
-      cliArgs.i18nLocation ?? configFile?.i18n_LOCATION ?? i18nLocationDefault,
-    translations:
-      cliArgs.translations ??
-      configFile?.TRANSLATIONS_LOCATION ??
-      translationsLocationDefault,
-    output:
-      cliArgs.output ?? configFile?.OUTPUT_LOCATION ?? outputLocationDefault,
-    quiet: cliArgs.quiet ?? configFile?.QUIET ?? quietDefault,
+    i18nLocation,
+    translations,
+    output,
+    filenameOverride,
+    quiet,
   };
 }
 
