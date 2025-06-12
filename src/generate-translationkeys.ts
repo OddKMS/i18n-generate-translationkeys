0a04fba143770ff33a6eff9b909fa00d1b3fbb2d
Odd Karsten Svartaas <oksvart@gmail.com>
2025-06-12 13:02:55 +0200

    Add 'verbose' flag check for additional output
    Change translations variable name to reflect name change in previous commit

--- src/generate-translationkeys.ts
+++ src/generate-translationkeys.ts
@@ -3,8 +3,14 @@ import * as path from 'path';
 import * as jq from 'node-jq';
 import { getConfiguration } from '#helpers';
 
-const { i18nLocation, translations, output, filenameOverride, quiet } =
-  getConfiguration();
+const {
+  i18nLocation,
+  translationsLocation,
+  output,
+  filenameOverride,
+  verbose,
+  quiet,
+} = getConfiguration();
 
 const readFiles = () => {
   //  Create a union of the different translation files.
@@ -20,14 +26,21 @@ const readFiles = () => {
 };
 
 const generate = () => {
-  console.log('--------------------------------------------------------------');
-  console.log('Running script with the following config:');
-  console.log('--------------------------------------------------------------');
-  console.log('i18n location:     ', i18nLocation);
-  console.log('translations:      ', translations);
-  console.log('output:            ', output);
-  console.log('filename override: ', filenameOverride);
-  console.log('quiet:             ', quiet);
+  if (verbose) {
+    console.log(
+      '--------------------------------------------------------------'
+    );
+    console.log('Running script with the following config:');
+    console.log(
+      '--------------------------------------------------------------'
+    );
+    console.log('i18n location:     ', i18nLocation);
+    console.log('translations:      ', translationsLocation);
+    console.log('output:            ', output);
+    console.log('filename override: ', filenameOverride);
+    console.log('verbose:           ', verbose);
+    console.log('quiet:             ', quiet);
+  }
 };
 
 export default generate;
